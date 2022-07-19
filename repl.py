5b08bdef3726abb015559c58324766b4e2e6be08
James Haller <jahaller@microsoft.com>
2022-07-19 11:53:42 -0700

    Add pct & cumpct

--- repl.py
+++ repl.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from numpy import cumsum
 from dice_stats import IDiceStats, DieStats
 from typing import Callable
 
@@ -15,6 +16,34 @@ def print_counts(d: IDiceStats) -> None:
 	for n in range(1, d.get_max()+1):
 		count = d.get_count(n)
 		print(f'{n:>2} : {count}')
+
+
+def print_percentages(d: IDiceStats) -> None:
+	print(d)
+	counts = [d.get_count(n) for n in range(1, d.get_max()+1)]
+	total = sum(counts)
+	for n, count in enumerate(counts):
+		n += 1
+		pct = count / total
+		print(f'{n:>2} : {pct:>6.2%}')
+
+
+def print_cum_percentages(d: IDiceStats) -> None:
+	print(d)
+
+	counts = [d.get_count(n) for n in range(1, d.get_max()+1)]
+	cum_counts = cumsum(counts)
+
+	total = sum(counts)
+
+	for n, cum_count in enumerate(cum_counts):
+		n += 1
+		if cum_count == 0:
+			dash = '-'.center(7, ' ')
+			print(f'{n:>2} : {dash}')
+		else:
+			pct = (total - cum_count + 1) / total
+			print(f'{n:>2} : {pct:>7.2%}')
 
 
 def print_table(d: IDiceStats) -> None:
@@ -51,6 +80,10 @@ def repl():
 		'm': print_matrix,
 		'counts': print_counts,
 		'c': print_counts,
+		'pct': print_percentages,
+		'p': print_percentages,
+		'cumpct': print_cum_percentages,
+		'cp': print_cum_percentages,
 	}
 
 	curr_display: CommandType = print_table
@@ -67,5 +100,9 @@ def repl():
 				d = eval_dice(cmd)
 				curr_display(d)
 
+			case _:
+				print(f'Command {cmd} not recognized')
 
-repl()
+
+if __name__ == '__main__':
+	repl()
